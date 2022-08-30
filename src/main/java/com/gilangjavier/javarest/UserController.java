3afd00a3e491ce963af2e0b8d99fc0b046baf533
gilangjavier <gillvision0@gmail.com>
2022-08-30 13:07:21 +0700

    Add

--- src/main/java/com/gilangjavier/javarest/UserController.java
+++ src/main/java/com/gilangjavier/javarest/UserController.java
@@ -1,19 +1,7 @@
-package com.gilangjavier.javarest;
+package main.java.com.gilangjavier.javarest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class JavarestApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JavarestApplication.class, args);
-	}
-}
-
-
 @RestController
 @RequestMapping("/user")
 public class UserController {
