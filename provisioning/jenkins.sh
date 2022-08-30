3d318b51c50c2c2231480ebc12907ee109397729
gilangjavier <gillvision0@gmail.com>
2022-08-30 12:20:45 +0700

    update jdk

--- provisioning/jenkins.sh
+++ provisioning/jenkins.sh
@@ -2,7 +2,7 @@
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
-sudo apt install openjdk-8-jdk -y
+sudo apt install openjdk-11-jdk -y
 sudo apt install maven -y
 sudo apt install jenkins -y
 sudo systemctl start jenkins
