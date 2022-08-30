1e52df449944569fe404d28ea74a11ce863abefd
gilangjavier <gillvision0@gmail.com>
2022-08-30 11:46:45 +0700

    add source code

---
+++ src/main/java/com/gilangjavier/javarest/JavarestApplication.java
@@ -0,0 +1,13 @@
+package com.gilangjavier.javarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarestApplication.class, args);
+	}
+
+}
