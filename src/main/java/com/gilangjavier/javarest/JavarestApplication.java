a3d4e824c02c2da23a84dc13b67dde9bf0b08558
gilangjavier <gillvision0@gmail.com>
2022-08-30 12:40:33 +0700

    Test

--- src/main/java/com/gilangjavier/javarest/JavarestApplication.java
+++ src/main/java/com/gilangjavier/javarest/JavarestApplication.java
@@ -1,5 +1,7 @@
 package com.gilangjavier.javarest;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,4 +11,26 @@ public class JavarestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavarestApplication.class, args);
 	}
+}
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    // Generate toString method
+}
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/read/one")
+    public String readUser(){
+        User user = new User("John","doe","John@javadev.com");
+        return user.toString();
+    }
 }
